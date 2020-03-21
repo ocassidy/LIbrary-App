e59e948b99f6c32a9d014b0ca1493ea503052583
Oisin Cassidy <oisin.c@hotmail.co.uk>
2020-03-21 19:48:33 +0000

    Integration test refactor

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,9 +1,27 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+const apiUrlEnvCheck = () => {
+  console.log(process.env.REACT_APP_ENV)
+  let tempUrl;
+  if (process.env.REACT_APP_ENV === 'development') {
+    tempUrl = 'http://localhost:3000';
+  } else if (process.env.REACT_APP_ENV === 'integration') {
+    tempUrl = 'https://integration-library-app.herokuapp.com';
+  }
+  return tempUrl;
+};
+
+const API_BASE_URL = apiUrlEnvCheck();
+
+setHeadlessWhen(process.env.REACT_APP_ENV !== 'development');
+console.log(setHeadlessWhen)
+
 exports.config = {
   tests: 'src/**/*.integrationTest.js',
   output: './output',
   helpers: {
     Nightmare: {
-      url: 'http://localhost:3000',
+      url: API_BASE_URL,
       show: false,
     },
   },
--- src/components/Auth/AuthFlow.integrationTest.js
+++ src/components/Auth/AuthFlow.integrationTest.js
@@ -1,26 +1,7 @@
-const path = require('path');
-
-const dotEnvPath = path.resolve('./.env');
-require('dotenv').config({ path: dotEnvPath });
-
-const apiUrlEnvCheck = () => {
-  let tempUrl;
-  if (process.env.REACT_APP_ENV === 'development') {
-    tempUrl = 'http://localhost:3000';
-  } else if (process.env.REACT_APP_ENV === 'integration') {
-    tempUrl = 'https://integration-library-app.herokuapp.com';
-  }
-  return tempUrl;
-};
-
-const API_BASE_URL = apiUrlEnvCheck();
-
-console.log('API_BASE_URL', API_BASE_URL);
-
 Feature('Auth flow');
 Scenario('Register/Login/Logout flow', (I) => {
   const username = `test${Math.random().toFixed(3)}`;
-  I.amOnPage(`${API_BASE_URL}/register`);
+  I.amOnPage('/register');
   I.see('Register');
   I.fillField('.registerUsernameInput', username);
   I.fillField('.registerFirstNameInput', 'testFirst name');
@@ -30,7 +11,7 @@ Scenario('Register/Login/Logout flow', (I) => {
   I.fillField('.registerRetypePasswordInput', 'password');
   I.click('Register');
   I.see('Login');
-  I.amOnPage(`${API_BASE_URL}/login`);
+  I.amOnPage('/login');
   I.see('Login');
   I.fillField('.loginUsernameInput', username);
   I.fillField('.loginPasswordInput', 'password');
